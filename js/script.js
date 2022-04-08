40aa8182b36e8d3475a783ae6867419962c43243
= <hudson@kenzie.com.br>
2022-04-08 11:27:05 -0300

    introducao javaScript

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let nomePessoa  = "Hudson"
+
+let numero2     = 10  
+
+const VALOR_FRETE =  50 
+
+let numero1     = 20
+
+
+//TIPO DE DADOS 
+//STRING
+let primeiroNome  = "Hudson"
+
+//INT
+let segundoNome =  "12"
+
+//FLOAT
+let numeroFloat = 12.05
+
+//BOOLEAN
+let verdadeiro = true
+let falso      = false
+
+
