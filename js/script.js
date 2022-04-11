1cd46a9254c60ef996498cbb2af306b20fdd3c88
Hudson Carolino <hudson@kenzie.com.br>
2022-04-11 11:02:12 -0300

    condicionais if/else

--- js/script.js
+++ js/script.js
@@ -22,3 +22,91 @@ let verdadeiro = true
 let falso      = false
 
 
+
+
+
+// let usuario  = "Hudson"
+// let senha    = "123457"
+
+ 
+// if(usuario == "Hudson" && senha == "12345"){
+
+//     console.log("Login com sucesso")
+
+
+// }else{
+//     console.log("Usuario ou senha invalida")
+// }
+
+
+// let nomeAdmin    = "Gabriel"
+
+
+// if(nomeAdmin == "Anderson" || nomeAdmin == "Hudson"){
+
+//     console.log("Login com sucesso")
+
+// }else{
+
+//     console.log("Usuario ou senha invalida")
+// }
+
+// let usuarioVip = false
+// let usuario    = "Gabriel"
+//  let senha      = "12345"
+
+// if(usuario == "Gabriel" && senha == "12345"){
+//     console.log("Login com sucesso")
+
+//     if(usuarioVip){
+        
+//         console.log("Usuario vip")
+        
+//     }else{
+
+//         console.log("Não é usuario vip")
+//     }
+
+
+// }else{
+
+//     console.log("Usuario ou senha invalida")
+// }
+
+
+
+// let numero = 10
+
+// if(typeof numero == "number"){
+
+//     console.log("Tipo numero")
+
+// }
+
+
+// let idade = 18
+
+// if(idade >= 18){
+//     console.log("Atingiu maior idade")
+// }else{
+//     console.log("Não atingiu maior idade")
+// }
+
+
+// let totalPedido = 250
+
+
+// if(totalPedido > 200){
+   
+//     let descontoFrete = 25
+//     console.log("Aplicou desconto 25")
+
+// }
+
+// if(totalPedido > 250){
+
+//     let descontoFrete = 50
+//     console.log("Aplicou desconto 50")
+
+// }
+
